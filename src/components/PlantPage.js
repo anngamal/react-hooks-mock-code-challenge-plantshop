93e63e23eb1fdb2ca186a79b754db17633fd396d
ann gamal <anngamal14@gmail.com>
2023-09-20 12:15:56 -0400

    plantSearch

--- src/components/PlantPage.js
+++ src/components/PlantPage.js
@@ -1,14 +1,29 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import NewPlantForm from "./NewPlantForm";
 import PlantList from "./PlantList";
 import Search from "./Search";
 
 function PlantPage() {
+  const [plants, setPlants]= useState([])
+  const [search, setSearch] = useState("")
+  const filteredPlants= plants.filter(plant =>{
+    return plant.name.toLowerCase().includes(
+      search.toLowerCase()
+    )
+  })
+  useEffect(()=>{
+    fetch(" http://localhost:6001/plants")
+    .then((res) => res.json())
+    .then((plants)=>setPlants(plants))
+    console.log(plants)
+  },[])
+ 
+
   return (
     <main>
-      <NewPlantForm />
-      <Search />
-      <PlantList />
+      <NewPlantForm setPlants={setPlants}/>
+      <Search setSearch={setSearch}/>
+      <PlantList plants={filteredPlants}/>
     </main>
   );
 }
